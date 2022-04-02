57c25c01f4be6376a1facdd0bfbb307135abbd7e
Salih KIR <salihkir2020@gmail.com>
2022-04-02 11:06:01 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Salih KIR on 2.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
